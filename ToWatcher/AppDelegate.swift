aba8ff6b25722dcf3294583a03b20c27a7c0e91f
Делин Александр <adelin@more.tv>
2021-04-11 15:06:18 +0300

    Add settings with feature to choose start screen

--- ToWatcher/AppDelegate.swift
+++ ToWatcher/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         try? Keychain().set(token, key: "token")
+        Settings.setAppInfo()
         
         return true
     }
